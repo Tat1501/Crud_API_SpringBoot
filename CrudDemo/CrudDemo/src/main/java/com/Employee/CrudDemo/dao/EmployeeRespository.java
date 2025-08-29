2ccae34c01229a2781b9cce6fac167a5e50c04ae
Tushar Tiwari <aayushtiwari2905@gmail.com>
2025-08-29 20:57:44 +0530

    pagination of rest api

--- CrudDemo/CrudDemo/src/main/java/com/Employee/CrudDemo/dao/EmployeeRespository.java
+++ CrudDemo/CrudDemo/src/main/java/com/Employee/CrudDemo/dao/EmployeeRespository.java
@@ -2,6 +2,13 @@ package com.Employee.CrudDemo.dao;
 
 import com.Employee.CrudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RepositoryRestResource(path="members")
+// this is used to change our endpoints from
+//http://localhost:8080/api/employees  ---- >>>  http://localhost:8080/api/members
+
 
 public interface EmployeeRespository extends JpaRepository<Employee, Integer> {
 
