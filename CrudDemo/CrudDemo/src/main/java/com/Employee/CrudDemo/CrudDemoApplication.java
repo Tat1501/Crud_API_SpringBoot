1195a4971eb6409a869e39415f65588e33105710
Tushar Tiwari <aayushtiwari2905@gmail.com>
2025-08-29 20:39:08 +0530

    spring data rest added
    deleted the services and rest controller package and also updated application properties for api request

--- CrudDemo/CrudDemo/src/main/java/com/Employee/CrudDemo/CrudDemoApplication.java
+++ CrudDemo/CrudDemo/src/main/java/com/Employee/CrudDemo/CrudDemoApplication.java
@@ -8,6 +8,10 @@ public class CrudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
+
+		// we have done the changes in application properties so that we can hit our api like - http://localhost:8080/api/employees
+		// if not then commit that change in application properties and then hit the url like http://localhost:8080/employees
+
 	}
 
 }
